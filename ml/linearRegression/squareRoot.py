f4ddee020506a0aad8f238921041ab847d1a5927
aidway <aidway@163.com>
2018-03-06 10:35:27 +0800

    v2.0

--- ml/linearRegression/squareRoot.py
+++ ml/linearRegression/squareRoot.py
@@ -5,18 +5,15 @@ import math
     版本：v1.0
     日期：20180303
     文件名：squareRoot.py
-    功能：使用梯度下降算法求正整数a的平方根
-    
+    功能：使用梯度下降算法求正整数1-100的平方根
 """
 
-learning_rate = 0.1
-a = 99
-x = 0
-for i in range(2000):
-    x -= learning_rate * (x**2 - a)
-print("预测值: %f  真实值: %f" %(x, math.sqrt(a)))
 
-
-
-
+if __name__ == "__main__":
+    learning_rate = 0.01
+    x = 0
+    for a in range(1, 101):
+        for i in range(2000):
+            x -= learning_rate * (x**2 - a)
+        print("数值:%d  预测平方根: %f  真实平方根: %f  差异值:%f" % (a, x, math.sqrt(a), math.sqrt(a) - x))
 
